52b240f6679d5a218d283595b074def58202e061
naoya-okamura <85878734+naoya-okamura@users.noreply.github.com>
2021-06-18 12:53:24 +0900

    button完了・削除完了

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  // テキストボックスの値を初期化する。
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  // div生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  // liタグ生成
+  const li = document.createElement("li");
+  li.innerText = inputText;
+
+  //未完了のリストに追加
+  document.getElementById("incomplete-list").appendChild(div);
+
+  //button(完了)タグ生成
+  const completebutton = document.createElement("button");
+  completebutton.innerText = "完了";
+
+  //button(削除)タグ生成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+
+  // divタグの子要素に各要素を設定
+  div.appendChild(li);
+  div.appendChild(completebutton);
+  div.appendChild(deleteButton);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
